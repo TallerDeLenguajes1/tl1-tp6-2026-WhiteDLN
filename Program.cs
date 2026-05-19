7d6b2aca849ae14e88d804b6c9d88d8fe5cebda1
WhiteDLN <white14dl@gmail.com>
2026-05-19 13:48:10 -0300

    Ejercicio 2 sin terminar

--- Program.cs
+++ Program.cs
@@ -27,3 +27,53 @@ else
 
 
 
+int numeroC1 = 0;
+int numeroC2 = 0;
+int operacion = 0;
+
+if (int.TryParse(Console.ReadLine(), out numeroC1) && int.TryParse(Console.ReadLine(), out numeroC2))
+{
+
+    bool OperacionValida = false;
+    OperacionValida = int.TryParse(Console.ReadLine(), out operacion);
+
+    
+    do
+    {
+        if (!OperacionValida)
+    {
+        Console.WriteLine("El valor ingresado no es un numero valido.");
+        return;
+    }
+
+    else {
+        Console.WriteLine("Ingrese la operacion a realizar: 1 para suma, 2 para resta, 3 para multiplicacion y 4 para division");
+        Console.ReadLine();
+        switch(operacion)
+        {
+            case 1:
+                Console.WriteLine("La suma es: " + (numeroC1 + numeroC2));
+                break;
+            case 2:
+                Console.WriteLine("La resta es: " + (numeroC1 - numeroC2));
+                break;
+            case 3:
+                Console.WriteLine("La multiplicacion es: " + (numeroC1 * numeroC2));
+                break;
+            case 4:
+                Console.WriteLine("La division es: " + (numeroC1 / numeroC2));
+                break;
+        }
+    }
+
+    }
+    while(operacion < 1 || operacion > 4);
+    
+}
+
+
+
+else
+{
+    Console.WriteLine("El valor ingresado no es un numero valido.");
+}
