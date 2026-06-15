baa6126e2f6622b1ee5a30cc9c5e40159d00d2b3
WhiteDLN <white14dl@gmail.com>
2026-06-15 20:47:27 -0300

    CalculadoraV1 pre Merge

--- Program.cs
+++ Program.cs
@@ -1,7 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int bandera = 0;
 int numero = 0;
-int resultado = 0;
+int resultadoI = 0;
+
+do {
+    int Eleccion;
+    Console.WriteLine("Seleccione la opcion que desea realizar:");
+    Console.WriteLine("[0]Salir");
+    Console.WriteLine("[1]Sumar");
+    Console.WriteLine("[2]Restar");
+    Console.WriteLine("[3]Multiplicar");
+    Console.WriteLine("[4]Dividir");
+    if(int.TryParse(Console.ReadLine(), out Eleccion) && Eleccion >= 0 && Eleccion <= 4) {
+
+        if(Eleccion == 0){
+            Console.WriteLine("Saliendo del programa...");
+            bandera = 1;
+        }
+
+        else{
+        int num1 = PedirNumero("Ingrese el primer valor:");
+        int num2; //RECUERDA INICIALIZAR
+        int resultado;
+
+        switch(Eleccion){
+            case 1:
+            num2 = PedirNumero("Ingrese el segundo valor:");
+            resultado = num1 + num2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+
+            case 2:
+            num2 = PedirNumero("Ingrese el segundo valor:");
+            resultado = num1 - num2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+
+            case 3:
+            num2 = PedirNumero("Ingrese el segundo valor:");
+            resultado = num1 * num2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+
+            case 4:
+            num2 = PedirDividendo("Ingrese un numero diferente a 0.");
+            resultado = num1 / num2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+
+        }
+    }
+
+    }
+
+}while (bandera == 0);
+
 
 Console.WriteLine("Ingrese un numero");
 
@@ -11,12 +63,12 @@ if(int.TryParse(Console.ReadLine(), out numero))
     Console.WriteLine("El numero ingresado es: " + numero);
     while (numero > 0)
 {
-    resultado = resultado * 10 + numero % 10;
+    resultadoI = resultadoI * 10 + numero % 10;
     
     numero = numero / 10;
 }
 
-Console.WriteLine("El numero invertido es: " + resultado);
+Console.WriteLine("El numero invertido es: " + resultadoI);
 
 
 }
@@ -26,4 +78,39 @@ else
 }   
 
 
+static int PedirNumero(string Mensaje){
+    int Numero;
+    bool Valido;
 
+    do {
+        Console.WriteLine(Mensaje);
+        Valido = int.TryParse(Console.ReadLine(), out Numero);
+
+        if (!Valido){
+            Console.WriteLine("Debe ingresar un numero valido.");
+        }
+    }while(!Valido);
+
+    return Numero;
+}
+
+static int PedirDividendo(string Mensaje){
+    int Numero;
+    bool Valido;
+
+    do {
+        Console.WriteLine(Mensaje);
+        Valido = int.TryParse(Console.ReadLine(), out Numero);
+
+        if (!Valido){
+            Console.WriteLine("Debe ingresar un numero valido.");
+        }
+
+        else if(Numero == 0){
+            Console.WriteLine("El divisor no puede ser 0.");
+            Valido = false;
+        }
+    }while(!Valido);
+
+    return Numero;
+}
