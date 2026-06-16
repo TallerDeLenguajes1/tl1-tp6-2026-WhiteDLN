00dbeac25fa96bdf1999eff454ddaad885e22026
WhiteDLN <white14dl@gmail.com>
2026-06-15 22:18:43 -0300

    V2 terminada

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hello, World!");
 int numero = 0;
 int resultado = 0;
@@ -27,3 +29,86 @@ else
 
 
 
+double ValorAMostrar = PedirNumeroDouble("Elija el valor que desee saber El valor absoluto de un número El cuadrado, La raíz cuadrada  El seno y El Coseno");
+double Absoluto, Cuadrado, Raiz;
+
+Absoluto = Math.Abs(ValorAMostrar);
+Console.WriteLine("El valor Absoluto es: " + Absoluto);
+
+Cuadrado = Math.Pow(ValorAMostrar, 2);
+Console.WriteLine("El cuadrado es: " + Cuadrado);
+
+if(ValorAMostrar >= 0){
+    Raiz = Math.Sqrt(ValorAMostrar);
+    Console.WriteLine("La raiz es: " + Raiz);
+}
+else {
+    Console.WriteLine("No se puedde sacar la raiz cuadrada de un negativo.");
+}
+
+double radianes = ValorAMostrar * Math.PI / 180.0;
+Console.WriteLine($"Seno: {Math.Sin(radianes)}");
+Console.WriteLine($"Coseno: {Math.Cos(radianes)}");
+
+float numeroFloat = PedirNumeroFloat("Ingrese un decimal.");
+double parteEntera = Math.Truncate(numeroFloat);
+Console.WriteLine("La parte entera de: " + numeroFloat + "es: " + parteEntera);
+
+
+
+
+
+
+static int PedirNumero(string Mensaje){
+    int Numero;
+    bool Valido;
+
+    do {
+        Console.WriteLine(Mensaje);
+        Valido = int.TryParse(Console.ReadLine(), out Numero);
+
+        if (!Valido){
+            Console.WriteLine("Debe ingresar un numero valido.");
+        }
+    }while(!Valido);
+
+    return Numero;
+}
+
+static double PedirNumeroDouble(string Mensaje)
+{
+    double Numero;
+    bool Valido;
+
+    do
+    {
+        Console.WriteLine(Mensaje);
+        Valido = double.TryParse(Console.ReadLine(), out Numero);
+
+        if (!Valido)
+        {
+            Console.WriteLine("Debe ingresar un numero valido.");
+        }
+    } while (!Valido);
+
+    return Numero;
+}
+
+static float PedirNumeroFloat(string Mensaje)
+{
+    float Numero;
+    bool Valido;
+
+    do
+    {
+        Console.WriteLine(Mensaje);
+        Valido = float.TryParse(Console.ReadLine(), out Numero);
+
+        if (!Valido)
+        {
+            Console.WriteLine("Debe ingresar un numero valido.");
+        }
+    } while (!Valido);
+
+    return Numero;
+}
